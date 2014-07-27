4176869fffb6be46da98531231cb4b8b22a876b5
Ben Drucker <bvdrucker@gmail.com>
2014-07-27 17:30:51 -0400

    Slim ConvexCache API and add full unit tests

--- src/cache.js
+++ src/cache.js
@@ -5,13 +5,6 @@ module.exports = function ($cacheFactory) {
   var ConvexCache = function (name) {
     this.$name = 'convex-' + name;
     this.$$cache = $cacheFactory(this.$name);
-    ConvexCache.caches[name] = this;
-  };
-
-  ConvexCache.caches = {};
-
-  ConvexCache.get = function (name) {
-    return this.caches[name];
   };
 
   ConvexCache.prototype.put = function (key, value) {
@@ -31,8 +24,7 @@ module.exports = function ($cacheFactory) {
   };
 
   ConvexCache.prototype.destroy = function () {
-    this.$$cache.destroy();
-    delete ConvexCache.caches[this.$name];
+    return this.$$cache.destroy();
   };
 
   return ConvexCache;
---
+++ test/unit/cache.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var angular = require('angular');
+var uuid    = require('node-uuid');
+
+describe('ConvexCache', function () {
+
+  var ConvexCache, $cacheFactory, cache, $$cache;
+  beforeEach(angular.mock.module(require('../../')));
+  beforeEach(angular.mock.inject(function ($injector) {
+    ConvexCache = $injector.get('ConvexCache');
+    $cacheFactory = $injector.get('$cacheFactory');
+  }));
+  beforeEach(function () {
+    cache = new ConvexCache('model');
+    $$cache = cache.$$cache;
+  });
+  afterEach(function () {
+    cache.destroy();
+  });
+
+  describe('Constructor', function () {
+
+    it('names the cache', function () {
+      expect(cache).to.have.property('$name', 'convex-model');
+    });
+
+    it('instantiates a cache with $cacheFactory', function () {
+      expect(cache)
+        .to.have.property('$$cache')
+        .that.equals($cacheFactory.get(cache.$name));
+    });
+
+  });
+
+  describe('#put', function () {
+
+    it('puts a key/value pair into the cache', function () {
+      expect(cache.put('key', 'value')).to.equal('value');
+      expect(cache.get('key')).to.equal('value');
+    });
+
+  });
+
+  describe('#get', function () {
+
+    it('gets a value from the cache by key', function () {
+      cache.put('key', 'value');
+      expect(cache.get('key')).to.equal('value');
+    });
+
+  });
+
+  describe('#remove', function () {
+
+    it('removes a value from the cache by key', function () {
+      cache.put('key', 'value');
+      cache.remove('key');
+      expect(cache.get('key')).to.be.undefined;
+    });
+
+  });
+
+  describe('#removeAll', function () {
+
+    it('removes all values from the cache', function () {
+      cache.put('key', 'value');
+      cache.removeAll();
+      expect(cache.get('key')).to.be.undefined;
+    });
+
+  });
+
+});
