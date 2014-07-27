a1ffc0303a1ed98543a5c37a26beeced5a9d80b2
Ben Drucker <bvdrucker@gmail.com>
2014-07-27 16:23:55 -0400

    Add coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -38,13 +38,21 @@ module.exports = function(config) {
 
     browserify: {
         debug: true,
+        transform: ['browserify-shim', 'browserify-istanbul']
+    },
+
+    coverageReporter: {
+        reporters: [
+            {type: 'html'},
+            {type: 'text-summary'}
+        ]
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
