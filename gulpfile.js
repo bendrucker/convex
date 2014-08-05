b2ebf8a111ee7ac228b81e0a6ec2424e2608bd0c
Ben Drucker <Ben@bendrucker.local>
2014-08-05 15:40:21 -0400

    Migrate repetitive tasks into gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var argv    = require('yargs').argv;
+
+gulp.task('lint', function () {
+  gulp.src(['./src/**/*.js', './test/**/*.js', './gulpfile.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.jshint.reporter('fail'));
+});
+
+gulp.task('test', function () {
+  return require('karma-as-promised').server.start({
+    frameworks: ['browserify', 'mocha', 'chai-sinon'],
+    files: [
+      'node_modules/es5-shim/es5-shim.js',
+      'node_modules/chai-as-promised/lib/chai-as-promised.js',
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'test/**/*.js': 'browserify'
+    },
+    browserify: {
+      debug: true,
+      transform: ['browserify-istanbul', 'browserify-shim']
+    },
+    coverageReporter: {
+      type: 'lcovonly',
+      dir: './coverage',
+      subdir: './'
+    },
+    reporters: ['progress', 'coverage'],
+    browsers: ['PhantomJS'],
+    singleRun: true
+  });
+});
+
+gulp.task('bump', function () {
+  gulp.src(['./package.json', './bower.json'])
+    .pipe(plugins.bump({
+      type: argv.type
+    }))
+    .pipe(gulp.dest('./'));
+});
