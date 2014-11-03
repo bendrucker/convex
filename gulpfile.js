0d577f6f08a12cc17c9ec15c3909737658b3f7b0
Ben Drucker <bvdrucker@gmail.com>
2014-11-03 10:28:52 -0500

    Run tests on Sauce Labs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var extend  = require('xtend');
 var argv    = require('yargs').argv;
 
 gulp.task('lint', function () {
@@ -11,19 +12,34 @@ gulp.task('lint', function () {
     .pipe(plugins.jshint.reporter('fail'));
 });
 
+var testConfig = {
+  frameworks: ['browserify', 'mocha', 'chai-sinon'],
+  files: [
+    'node_modules/es5-shim/es5-shim.js',
+    'node_modules/chai-as-promised/lib/chai-as-promised.js',
+    'components/angular/angular.js',
+    'components/angular-mocks/angular-mocks.js',
+    'test/**/*.js'
+  ],
+  preprocessors: {
+    'test/**/*.js': 'browserify'
+  },
+  browserify: {
+    debug: true,
+    transform: ['browserify-shim']
+  },
+  coverageReporter: {
+    type: 'html',
+    dir: './coverage',
+    subdir: './'
+  },
+  reporters: ['progress', 'coverage'],
+  browsers: ['PhantomJS'],
+  singleRun: !!process.env.CI
+};
+
 gulp.task('test', function () {
-  return require('karma-as-promised').server.start({
-    frameworks: ['browserify', 'mocha', 'chai-sinon'],
-    files: [
-      'node_modules/es5-shim/es5-shim.js',
-      'node_modules/chai-as-promised/lib/chai-as-promised.js',
-      'components/angular/angular.js',
-      'components/angular-mocks/angular-mocks.js',
-      'test/**/*.js'
-    ],
-    preprocessors: {
-      'test/**/*.js': 'browserify'
-    },
+  return require('karma-as-promised').server.start(extend({}, testConfig, {
     browserify: {
       debug: true,
       transform: ['browserify-istanbul', 'browserify-shim']
@@ -32,11 +48,26 @@ gulp.task('test', function () {
       type: 'html',
       dir: './coverage',
       subdir: './'
+    }
+  }));
+});
+
+var launchers = require('./sauce.json');
+gulp.task('sauce', function () {
+  return require('karma-as-promised').server.start(extend({}, testConfig, {
+    sauceLabs: {
+      testName: 'Convex Unit Tests',
+      startConnect: false,
+      recordScreenshots: false,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
     },
-    reporters: ['progress', 'coverage'],
-    browsers: ['PhantomJS'],
-    singleRun: !!process.env.CI
-  });
+    transports: ['xhr-polling'],
+    captureTimeout: 0,
+    browserNoActivityTimeout: 120000,
+    reporters: ['dots', 'saucelabs'],
+    browsers: Object.keys(launchers),
+    customLaunchers: launchers
+  }));
 });
 
 gulp.task('bump', function () {
